b4138c98c73bdb285c47b7cd401c480f614d57ce
joo-seongchan <jskjsj601@gmail.com>
2022-06-14 13:06:31 +0900

    tag rule

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Fragment } from "react";
 
 const App = () => {
-  return <div>I kill you</div>
-}
+  return (
+    <>
+      <h1>gat</h1>
+      <h2>멀다멀어</h2>
+      <h3>갈길이</h3>
+      <img src="" />
+    </>
+  );
+};
+
 export default App;
