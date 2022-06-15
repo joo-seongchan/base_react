abd70a65e86abe1befc828bf7504a7c7a2dfc93a
joo-seongchan <jskjsj601@gmail.com>
2022-06-15 09:44:35 +0900

    props_1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,18 @@ const App = () => {
       {/* <FnComponents_1 />
       <FnComponents_2 /> */}
       {/* <ClassComponent /> */}
-      <FoodMenu />
+
+      {/* *props
+      =><컴포넌트 프롭스명 = "값"/> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" drink="콜라" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="에슐리" drink="환타" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="김치찌개" drink="식혜" />
+      {/* 2일차 */}
     </div>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,12 @@
-export const FoodMenu = () => {
-  return <h3>오느르으이 메뉴는 텐동</h3>;
+export const FoodMenu = ({ menuName, drink }) => {
+  // console.log(props);
+  // =>프롭스는 오브젝트형태로 가져오기 때문에 매개변수에 중괄호를 적고 작성할 것
+  console.log(menuName);
+  console.log(drink);
+  return (
+    <div>
+      오늘의 메뉴는 <b>{menuName}</b> 사이드 메뉴는 <b>{drink}</b>
+      {/* jsx안쪽에 변수를 사용할시 중괄호로 감싸줄것 */}
+    </div>
+  );
 };
