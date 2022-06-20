a038016db09586c485dff3eb4546c20bffda6b2f
joo-seongchan <jskjsj601@gmail.com>
2022-06-20 10:31:54 +0900

    title-style

--- src/components/5_MusicApp/components/TitleWrap.js
+++ src/components/5_MusicApp/components/TitleWrap.js
@@ -1,10 +1,22 @@
 import styled from "styled-components";
-const STitleWrap = styled.div``;
+const STitleWrap = styled.div`
+  text-align: center;
+  h3 {
+    font-size: 22px;
+    font-weight: 700;
+    margin-bottom: 5px;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 100;
+    opacity: 0.7;
+  }
+`;
 export const TitleWrap = () => {
   return (
     <STitleWrap>
-      <h3>Blue bird</h3>
-      <p>Pick Dreams</p>
+      <h3>JOKER</h3>
+      <p>Joker</p>
     </STitleWrap>
   );
 };
