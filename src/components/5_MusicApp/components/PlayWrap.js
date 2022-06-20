5f437ecab4ac192048deb76552c55c8d447420b2
joo-seongchan <jskjsj601@gmail.com>
2022-06-20 10:39:05 +0900

    Play-style

--- src/components/5_MusicApp/components/PlayWrap.js
+++ src/components/5_MusicApp/components/PlayWrap.js
@@ -1,8 +1,30 @@
 import styled from "styled-components";
 
-const SPlayWrap = styled.div``;
-const GageWrap = styled.div``;
-const Gage = styled.div``;
+const SPlayWrap = styled.div`
+  width: 100%;
+  margin-top: 30px;
+`;
+const GageWrap = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+`;
+const Gage = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: aquamarine;
+`;
+const TimeWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  span {
+    font-size: 12px;
+    font-weight: 100;
+    color: gray;
+    opacity: 0.7;
+  }
+`;
 
 export const PlayWrap = () => {
   return (
@@ -10,8 +32,10 @@ export const PlayWrap = () => {
       <GageWrap>
         <Gage />
       </GageWrap>
-      <span>00:03</span>
-      <span>03:40</span>
+      <TimeWrap>
+        <span>00:03</span>
+        <span>03:40</span>
+      </TimeWrap>
     </SPlayWrap>
   );
 };
