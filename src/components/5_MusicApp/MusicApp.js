1685e33d0d8830656e23e2aacb4d39d0c70847fc
joo-seongchan <jskjsj601@gmail.com>
2022-06-20 10:27:11 +0900

    cover-style

--- src/components/5_MusicApp/MusicApp.js
+++ src/components/5_MusicApp/MusicApp.js
@@ -20,15 +20,30 @@ const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #555;
+  color: white;
 `;
 const Section = styled.div`
   width: 375px;
   height: 667px;
-  background-color: #333;
-  margin: 0 auto;
+  background-color: #1d1d1d;
+  border-radius: 25px;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const CoverImg = styled.div``;
+const CoverImg = styled.div`
+  width: 90%;
+  height: 280px;
+  border-radius: 25px;
+  box-shadow: 0 15px 20px 10px rgba(0, 0, 0, 0.3);
+  margin: 30px 0;
+`;
 
 export const MusicApp = () => {
   return (
@@ -36,7 +51,11 @@ export const MusicApp = () => {
       <Section>
         <Header />
 
-        <CoverImg />
+        <CoverImg
+          style={{
+            background: `url(https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/guest/image/Kwrk5rrX-e3llXZKRRq3gMUszBs.JPG) no-repeat center / cover `,
+          }}
+        />
 
         <TitleWrap />
 
--- src/components/5_MusicApp/components/Header.js
+++ src/components/5_MusicApp/components/Header.js
@@ -3,13 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 const SHeader = styled.header`
+  width: 100%;
   display: flex;
   justify-content: space-between;
 `;
 const Menus = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
   display: flex;
-  justify-content: cetner;
+  justify-content: center;
   align-items: center;
+  &:nth-child(1) {
+    background-color: white;
+    color: #1d1d1d;
+    font-size: 12px;
+  }
+  &:nth-child(2) {
+    border: 1px solid #fff;
+  }
 `;
 
 export const Header = () => {
